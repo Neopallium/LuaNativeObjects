5512aaa6084f3bbfdbafdbeeb81e6177213e436a
Robert G. Jakabosky <bobby@sharedrealm.com>
2012-08-06 16:11:32 -0700

    Remove unused header file references.

--- project_template/cmake/LuaNativeObjects.cmake
+++ project_template/cmake/LuaNativeObjects.cmake
@@ -12,15 +12,13 @@ macro(GenLuaNativeObjects _src_files_var)
 	foreach(_src_file ${${_src_files_var}})
 		if(_src_file MATCHES ".nobj.lua")
 			string(REGEX REPLACE ".nobj.lua" ".nobj.c" _src_file_out ${_src_file})
-			string(REGEX REPLACE ".nobj.lua" ".nobj.h" _header_file_out ${_src_file})
 			string(REGEX REPLACE ".nobj.lua" ".nobj.ffi.lua" _ffi_file_out ${_src_file})
-			add_custom_command(OUTPUT ${_src_file_out} ${_header_file_out} ${_ffi_file_out}
+			add_custom_command(OUTPUT ${_src_file_out} ${_ffi_file_out}
 				COMMAND lua ${LUA_NATIVE_OBJECTS_PATH}/native_objects.lua -outpath ${CMAKE_CURRENT_BINARY_DIR} -gen lua ${_src_file}
 				WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
 				DEPENDS ${_src_file}
 			)
 			set_source_files_properties(${_src_file_out} PROPERTIES GENERATED TRUE)
-			set_source_files_properties(${_header_file_out} PROPERTIES GENERATED TRUE)
 			set_source_files_properties(${_ffi_file_out} PROPERTIES GENERATED TRUE)
 			string(REGEX REPLACE ".nobj.lua" "" _doc_base ${_src_file})
 			string(REGEX REPLACE ".nobj.lua" ".luadoc" _doc_file_out ${_src_file})
