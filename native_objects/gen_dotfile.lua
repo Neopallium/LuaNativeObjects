e615615824937d09aff9f7e13b8f48f22cb9959f
Aki <please@ignore.pl>
2024-09-04 23:06:09 +0200

    Rename dotlua to dottify
    This is closer to original source name and already used import name.
    Examples directory has been dusted off to generate png files for
    existing samples.

--- native_objects/gen_dotfile.lua
+++ native_objects/gen_dotfile.lua
@@ -18,7 +18,7 @@
 -- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 -- THE SOFTWARE.
 
-dottify = require"dotlua"
+local dottify = require"dottify"
 
 --
 -- dump records
